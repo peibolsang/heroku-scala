423961a9268cce26bdb52e9e265657ab6f99fc98
Pablo Bermejo <peibolsang@gmail.com>
2013-03-12 21:50:50 +0100

    Vamos a probar con MySQL en vez de Postgres

--- project/Build.scala
+++ project/Build.scala
@@ -9,8 +9,8 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      //  "mysql" % "mysql-connector-java" % "5.1.18" // THIS is for local
-        "postgresql" % "postgresql" % "8.4-702.jdbc4" // THIS is for Heroku until I find how to set up MySQL
+      "mysql" % "mysql-connector-java" % "5.1.18" // THIS is for local and Heroku with ClearDB
+      // "postgresql" % "postgresql" % "8.4-702.jdbc4" // THIS is for Heroku until I find how to set up MySQL
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
